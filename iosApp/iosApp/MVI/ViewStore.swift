db43e12aeee47a7c5eecf763b6fa53d5ad205260
moto0000 <moto0095@gmail.com>
2020-10-29 14:48:42 +0100

    Add `onEvent` func

--- iosApp/iosApp/MVI/ViewStore.swift
+++ iosApp/iosApp/MVI/ViewStore.swift
@@ -85,3 +85,59 @@ extension View {
         }
     }
 }
+
+fileprivate struct OnChangeViewModifier<Value>: ViewModifier where Value: Equatable {
+
+    @State var initialValue: Value
+
+    let value: Value
+
+    let action: (Value) -> Void
+    
+    func body(content: Content) -> some View {
+        if initialValue != value {
+            DispatchQueue.main.async {
+                initialValue = value
+                action(value)
+            }
+        }
+        
+        return content.onAppear()
+    }
+}
+
+extension View {
+
+    @_disfavoredOverload
+    @ViewBuilder
+    func onChange<Value>(
+        of value: Value,
+        perform action: @escaping (Value) -> Void
+    ) -> some View where Value: Equatable {
+        if #available(iOS 14.0, *) {
+            onChange(of: value, perform: action)
+        } else {
+            modifier(
+                OnChangeViewModifier(
+                    initialValue: value,
+                    value: value,
+                    action: action
+                )
+            )
+        }
+    }
+}
+
+extension View {
+
+    func onEvent<Value>(
+        _ event: MviEventRaw<Value>?,
+        perform action: @escaping (Value) -> Void
+    ) -> some View {
+        onChange(of: event) { event in
+            if let event = event, let value = event.value {
+                action(value)
+            }
+        }
+    }
+}
