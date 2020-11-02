2042ef9716119690041ece381b2fded69cde4ac4
Mariusz Dabrowski <mobite.app@gmail.com>
2020-11-02 17:12:13 +0100

    Remove unused import

--- shared/src/iosMain/kotlin/pl/mobite/playground/domain/home/mvi/impl/HomeStore.kt
+++ shared/src/iosMain/kotlin/pl/mobite/playground/domain/home/mvi/impl/HomeStore.kt
@@ -1,6 +1,5 @@
 package pl.mobite.playground.domain.home.mvi.impl
 
-import kotlinx.coroutines.MainScope
 import org.koin.core.inject
 import org.koin.core.parameter.parametersOf
 import pl.mobite.playground.common.MviStore
