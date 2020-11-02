0e7d88c31912a285abb968b840818c085205e759
moto0000 <moto0095@gmail.com>
2020-11-02 14:13:35 +0100

    Remove bindings from ViewStore (#16)
    * Remove bindings from ViewStore
    
    * SingleEvent -> Event
    
    * Small refactor
    
    * Small refactor
    
    * Remove whitespaces

--- iosApp/iosApp/HomeView.swift
+++ iosApp/iosApp/HomeView.swift
@@ -6,61 +6,61 @@ struct HomeView: View {
 
     typealias Store = AnyStore<HomeAction, HomeViewState>
 
-    private let store: Store
+    @ObservedObject private var viewStore: ViewStore<HomeAction, HomeViewState>
 
     @State private var text: String = ""
 
+    @State private var error: Event<KotlinThrowable>?
+
     init(store: Store = .store(from: HomeViewModel())) {
-        self.store = store
+        self.viewStore = .init(store: store)
     }
 
     var body: some View {
-        WithViewStore(store) { viewStore in
-            ZStack {
-                VStack {
-                    HStack {
-                        TextField("Add new task", text: $text)
-                        Button {
-                            viewStore.accept { $0.addTask(taskContent: text) }
-                        } label: {
-                            Text("Add")
-                        }
+        ZStack {
+            VStack {
+                HStack {
+                    TextField("Add new task", text: $text)
+                    Button {
+                        viewStore.accept { $0.addTask(taskContent: text) }
+                    } label: {
+                        Text("Add")
                     }
-                    .padding()
-                    Divider()
-                    Button("Delete completed Tasks") {
-                        viewStore.accept { $0.deleteCompletedTasks() }
-                    }
-                    Divider()
-                    List(viewStore.tasks ?? []) { task in
-                        Button {
-                            viewStore.accept { $0.updateTask(taskId: task.id, isDone: !task.isDone) }
-                        } label: {
-                            HStack {
-                                Text("\(task.content)")
-                                Spacer()
-                                Image(systemName: task.isDone ? "checkmark.circle.fill" : "circle")
-                            }
+                }
+                .padding()
+                Divider()
+                Button("Delete completed Tasks") {
+                    viewStore.accept { $0.deleteCompletedTasks() }
+                }
+                Divider()
+                List(viewStore.tasks ?? []) { task in
+                    Button {
+                        viewStore.accept { $0.updateTask(taskId: task.id, isDone: !task.isDone) }
+                    } label: {
+                        HStack {
+                            Text("\(task.content)")
+                            Spacer()
+                            Image(systemName: task.isDone ? "checkmark.circle.fill" : "circle")
                         }
                     }
                 }
             }
-            .navigationBarTitle("Tasks", displayMode: .inline)
-            .navigationBarItems(
-                leading: viewStore.inProgress ? AnyView(ActivityIndicator()) : AnyView(EmptyView())
-            )
-            .onAppear {
-                viewStore.accept { $0.loadDataIfNeeded() }
-            }
-            .alert(
-                item: viewStore.binding(for: \.errorEvent),
-                content: { event in
-                    Alert(title: Text(event.value.message ?? ""))
-                }
-            )
-            .onEvent(viewStore.taskAddedEvent) { _ in
-                text.removeAll()
-            }
+        }
+        .navigationBarTitle("Tasks", displayMode: .inline)
+        .navigationBarItems(
+            leading: viewStore.inProgress ? AnyView(ActivityIndicator()) : AnyView(EmptyView())
+        )
+        .onAppear {
+            viewStore.accept { $0.loadDataIfNeeded() }
+        }
+        .alert(item: $error) { event in
+            Alert(title: Text(event.message ?? ""))
+        }
+        .onEvent(viewStore.taskAddedEvent) { _ in
+            text.removeAll()
+        }
+        .onEvent(viewStore.errorEvent) { error in
+            self.error = .init(error)
         }
     }
 }
--- iosApp/iosApp/MVI/Events/MviEvent+Identifiable.swift
+++
@@ -1,5 +0,0 @@
-import class shared.MviEvent
-
-extension MviEvent: Identifiable {
-    // no-op
-}
