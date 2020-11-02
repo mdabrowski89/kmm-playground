46a4c35890c08f823abdf59738de7f17c0fd0cb6
krzysiek <krzysiek.zgondek@gmail.com>
2020-11-02 18:19:45 +0100

    ios fix

--- shared/src/iosMain/kotlin/pl/mobite/playground/common/Store.kt
+++ shared/src/iosMain/kotlin/pl/mobite/playground/common/Store.kt
@@ -7,23 +7,23 @@ import pl.mobite.playground.common.mvi.api.MviAction
 import pl.mobite.playground.common.mvi.api.MviResult
 import pl.mobite.playground.common.mvi.api.MviViewState
 
-abstract class Store<A: Any, VS: Any> {
+abstract class Store<A : Any, VS : Any> {
     abstract val initialState: VS
     abstract fun stateObserver(observer: (VS) -> Unit)
     abstract fun dispatch(intent: (VS) -> A?)
     abstract fun dispose()
 }
 
-abstract class MviStore<A: MviAction, R: MviResult, VS: MviViewState>(
+abstract class MviStore<A : MviAction, R : MviResult, VS : MviViewState>(
     internal val storeScope: CoroutineScope = MainScope()
-): KoinComponent, Store<A, VS>() {
+) : KoinComponent, Store<A, VS>() {
     abstract val mviController: MviController<A, R, VS>
 
     override val initialState: VS
         get() = mviController.initialViewState
 
     override fun stateObserver(observer: (VS) -> Unit) {
-        mviController.viewStatesFlow.watch(observer)
+        mviController.viewStatesFlow.asCommonFlow(storeScope).watch(observer)
     }
 
     override fun dispatch(intent: (VS) -> A?) {
@@ -33,9 +33,9 @@ abstract class MviStore<A: MviAction, R: MviResult, VS: MviViewState>(
     override fun dispose() = storeScope.cancel()
 }
 
-class EmptyStore<A: MviAction, VS: MviViewState>(
+class EmptyStore<A : MviAction, VS : MviViewState>(
     override val initialState: VS
-): Store<A, VS>() {
+) : Store<A, VS>() {
 
     override fun stateObserver(observer: (VS) -> Unit) {
         // no-op
--- shared/src/iosMain/kotlin/pl/mobite/playground/domain/home/mvi/impl/HomeStore.kt
+++ shared/src/iosMain/kotlin/pl/mobite/playground/domain/home/mvi/impl/HomeStore.kt
@@ -1,11 +1,10 @@
 package pl.mobite.playground.domain.home.mvi.impl
 
-import kotlinx.coroutines.MainScope
 import org.koin.core.inject
 import org.koin.core.parameter.parametersOf
 import pl.mobite.playground.common.MviStore
 import pl.mobite.playground.domain.home.mvi.HomeMviController
 
-class HomeStore: MviStore<HomeAction, HomeResult, HomeViewState>() {
+class HomeStore : MviStore<HomeAction, HomeResult, HomeViewState>() {
     override val mviController: HomeMviController by inject { parametersOf(storeScope) }
 }
