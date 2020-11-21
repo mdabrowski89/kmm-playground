8a25a8fe8c8eb4a1194515b119e89531ccc724a5
Mariusz Dabrowski <mobite.app@gmail.com>
2020-11-21 22:26:59 +0100

    Update gradle dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -19,12 +19,12 @@ dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 
-    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.core:core-ktx:1.3.2")
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.1")
 
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
